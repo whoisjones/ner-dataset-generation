52cc4f22562ae4ad8eb0d4b2a42b81cb79b0cf71
whoisjones <jonas.golde@gmail.com>
2025-11-13 16:21:56 +0100

    adding initial contrastive loss

--- src/loss.py
+++ src/loss.py
@@ -2,14 +2,17 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
+
 class BCELoss(nn.Module):
     def forward(self, logits, labels):
         loss = F.binary_cross_entropy_with_logits(
             logits, 
-            labels, 
+            labels,
+            pos_weight=torch.tensor(4.0, device=logits.device),
             reduction="none"
         )
         return loss
+
 
 class FocalLoss(nn.Module):
     def __init__(self, alpha=0.5, gamma=1.0):
@@ -31,6 +34,50 @@ class FocalLoss(nn.Module):
             loss = alpha_t * loss
 
         return loss
+
+
+class ContrastiveLoss(nn.Module):
+    def forward(
+        self, 
+        scores: torch.tensor, 
+        positions: list[int], 
+        mask: torch.tensor, 
+        prob_mask: torch.tensor = None
+    ) -> torch.tensor:
+        batch_size, seq_length = scores.size(0), scores.size(1)
+        if len(scores.shape) == 3:
+            scores = scores.view(batch_size, -1)
+            mask = mask.view(batch_size, -1)
+            log_probs = self.masked_log_softmax(scores, mask)
+            log_probs = log_probs.view(batch_size, seq_length, seq_length)
+            start_positions, end_positions = positions
+            batch_indices = list(range(batch_size))
+            log_probs = log_probs[batch_indices, start_positions, end_positions]
+        else:
+            log_probs = self.masked_log_softmax(scores, mask)
+            batch_indices = list(range(batch_size))
+            log_probs = log_probs[batch_indices, positions]
+        if prob_mask is not None:
+            log_probs = log_probs * prob_mask
+        return - log_probs.mean()
+
+    def masked_log_softmax(self, vector: torch.tensor, mask: torch.tensor, dim: int = -1) -> torch.tensor:
+        if mask is not None:
+            while mask.dim() < vector.dim():
+                mask = mask.unsqueeze(1)
+            vector = vector + (mask + self.tiny_value_of_dtype(vector.dtype)).log()
+        return torch.nn.functional.log_softmax(vector, dim=dim)
+
+    def tiny_value_of_dtype(self, dtype: torch.dtype) -> float:
+        if not dtype.is_floating_point:
+            raise TypeError("Only supports floating point dtypes.")
+        if dtype == torch.float or dtype == torch.double:
+            return 1e-13
+        elif dtype == torch.half:
+            return 1e-4
+        else:
+            raise TypeError("Does not support dtype " + str(dtype))
+
 
 class JGMakerLoss(nn.Module):
     def __init__(self, total_steps: int, k: float = 0.01, threshold: float = 0.5):
