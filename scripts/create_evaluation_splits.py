a045757ca75453388a870b9f5cec04f0f9c08726
whoisjones <jonas.golde@gmail.com>
2025-12-02 10:14:52 +0100

    script for creating the evaluation splits

---
+++ scripts/create_evaluation_splits.py
@@ -0,0 +1,42 @@
+import glob
+import os
+from datasets import DatasetDict
+
+NEW_EVAL_DATA_DIR = "/vol/tmp/goldejon/ner/eval_data"
+
+def convert_batch(batch):
+    # batch is a dict of lists: batch["token_spans"][i] is the list for sample i
+    token_spans = []
+    char_spans = []
+
+    for ts, cs in zip(batch["spans_tokens"], batch["spans_char"]):
+        token_spans.append(
+            [{"start": a["start"], "end": a["end"], "label": a["tag"]} for a in ts]
+        )
+        char_spans.append(
+            [{"start": a["start"], "end": a["end"], "label": a["tag"]} for a in cs]
+        )
+
+    batch["token_spans"] = token_spans
+    batch["char_spans"] = char_spans
+    return batch
+
+def main():
+    for file in glob.glob("/vol/tmp/goldejon/multilingual_ner/data/evaluation/*"):
+        for language_dataset in glob.glob(file + "/*"):
+            output_path = os.path.join(NEW_EVAL_DATA_DIR, language_dataset.split("/")[-2], language_dataset.split("/")[-1])
+            os.makedirs(output_path, exist_ok=True)
+            dataset = DatasetDict.load_from_disk(language_dataset)
+            dataset = dataset.map(
+                convert_batch,
+                batched=True,
+                num_proc=1,
+                desc="Converting spans",
+            )
+
+            dataset = dataset.remove_columns(["ner_tags_idx", "ner_tags_str", "spans_tokens", "spans_char"])
+            dataset.save_to_disk(output_path)
+
+
+if __name__ == "__main__":
+    main()
